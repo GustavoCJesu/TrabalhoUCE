955ad0bc9b607e024e4adbecc24eff4b78e1f996
GustavoJesuino <gustavo.jesuino1129@gmail.com>
2026-04-28 20:26:19 -0300

    Tela Profile pronta

--- app/(profile)/ProfileScreen.tsx
+++ app/(profile)/ProfileScreen.tsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, Image, StyleSheet, Text, View } from 'react-native'; // Correto
+import { MaterialIcons } from '@expo/vector-icons';
 
 const CardPessoa = ({ titulo, pessoa, pessoaEmail }: { titulo: string, pessoa: string, pessoaEmail: string }) => {
-        return (
-            <View style={styles.itemCard}>
-                <Text style={styles.subtitulo}>{titulo}</Text>
-                <View style={{ flexDirection: 'row', marginTop: 20, alignItems: 'center' }}>
-                    <Image
-                        style={styles.imgCard}
-                        source={require('../../assets/images/icon.png')}
-                        resizeMode="contain"
-                    />
-                    <View style={{ marginLeft: 20 }}>
-                        <Text style={styles.subtitulo}>{pessoa}</Text>
-                        <Text style={styles.txt}>{pessoaEmail}</Text>
-                    </View>
+    return (
+        <View style={styles.itemCard}>
+            <Text style={styles.subtitulo}>{titulo}</Text>
+            <View style={{ flexDirection: 'row', marginTop: 20, alignItems: 'center' }}>
+                <Image
+                    style={styles.imgCard}
+                    source={require('../../assets/images/icon.png')}
+                    resizeMode="contain"
+                />
+                <View style={{ marginLeft: 20, width: '80%' }}>
+                    <Text style={styles.subtitulo}>{pessoa}</Text>
+                    <Text style={styles.txt}>{pessoaEmail}</Text>
                 </View>
             </View>
-        )
-    }
+        </View>
+    )
+}
 
-    
+
 
 export default function Profile() {
     const [id, setID] = useState('')
@@ -30,35 +31,95 @@ export default function Profile() {
     const [coord, setCoord] = useState('')
     const [coordEmail, setCoordEmail] = useState('')
 
+    const percent = 50
+
     useEffect(() => {
 
         setFisio('Doutora')
         setFisioEmail('Especialista Ortopedica')
 
-    })
+        setCoord('Coordenadora')
+        setCoordEmail('Coordenadora do Curso de Fisioterapira')
+
+    }, [])
 
 
     return (
-        <View style={styles.container}>
-            <View style={styles.card}>
-                <Text style={styles.titulo}>Unifae Care</Text>
-                <View style={{ alignItems: 'center' }}>
-                    <Image style={styles.imgPerfil} source={require('../../assets/images/splash-icon.png')} resizeMode="contain" />
-                    <Text style={[styles.titulo, { marginTop: 20 }]}>{user}</Text>
-                    <Text style={{ fontSize: 16, color: '#10B981' }}>ID: {id}</Text>
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.card}>
+                    <Text style={styles.titulo}>Unifae Care</Text>
+                    <View style={{ alignItems: 'center' }}>
+                        <Image style={styles.imgPerfil} source={require('../../assets/images/splash-icon.png')} resizeMode="contain" />
+                        <Text style={[styles.titulo, { marginTop: 20 }]}>{user}</Text>
+                        <Text style={{ fontSize: 16, color: '#10B981' }}>ID: {id}</Text>
+                    </View>
+                    <CardPessoa
+                        titulo="Fisioterapeuta Responsável"
+                        pessoa={fisio}
+                        pessoaEmail={fisioEmail}
+                    />
+                    <CardPessoa
+                        titulo="Coordenador Responsável"
+                        pessoa={coord}
+                        pessoaEmail={coordEmail}
+                    />
+                    <View style={styles.itemCard}>
+                        <View style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+                            <Text style={{ color: 'white', fontSize: 18 }}>
+                                META SEMANAL
+                            </Text>
+                            <Text style={{ fontSize: 30, color: 'white' }}>
+                                {percent}%
+                                <Text style={{ fontSize: 15 }}> Concluido</Text>
+                            </Text>
+                            <View style={{ width: '100%', height: 10, backgroundColor: '#fff', borderRadius: 20, overflow: 'hidden' }}>
+                                <View style={{ width: `${percent}%`, height: '100%', backgroundColor: 'blue', borderRadius: 20 }}></View>
+                            </View>
+                        </View>
+                    </View>
+                    <View>
+                        <View style={styles.itemCard}>
+                            <Text style={{ color: 'white', fontSize: 20, marginBottom: 20 }}>
+                                Configurações de suporte
+                            </Text>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10, backgroundColor: '#464c55', padding: 10, marginTop: 10, borderRadius: 10, justifyContent: 'space-between', paddingHorizontal: 20 }}>
+                                <Image
+                                    style={{ width: 50, height: 50, borderRadius: 100 }}
+                                    source={require('../../assets/images/icon.png')}
+                                    resizeMode="contain"
+                                />
+                                <Text style={{ fontSize: 16, color: 'white' }}>Lembrete</Text>
+                                <Text style={{ fontSize: 30, color: 'white' }}><MaterialIcons name="arrow-forward-ios" size={20} color="#9CA3AF" /></Text>
+                            </View>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10, backgroundColor: '#464c55', padding: 10, marginTop: 10, borderRadius: 10, justifyContent: 'space-between', paddingHorizontal: 20 }}>
+                                <Image
+                                    style={{ width: 50, height: 50, borderRadius: 100 }}
+                                    source={require('../../assets/images/icon.png')}
+                                    resizeMode="contain"
+                                />
+                                <Text style={{ fontSize: 16, color: 'white' }}>Notificações</Text>
+                                <Text style={{ fontSize: 30, color: 'white' }}><MaterialIcons name="arrow-forward-ios" size={20} color="#9CA3AF" /></Text>
+                            </View>
+                            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 10, backgroundColor: '#464c55', padding: 10, marginTop: 10, borderRadius: 10, justifyContent: 'space-between', paddingHorizontal: 20 }}>
+                                <Image
+                                    style={{ width: 50, height: 50, borderRadius: 100 }}
+                                    source={require('../../assets/images/icon.png')}
+                                    resizeMode="contain"
+                                />
+                                <Text style={{ fontSize: 16, color: 'white' }}>Privacidade e Dados</Text>
+                                <Text style={{ fontSize: 30, color: 'white' }}><MaterialIcons name="arrow-forward-ios" size={20} color="#9CA3AF" /></Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{backgroundColor: '#f0a6a6', width: '80%', alignSelf: 'center', margin: 20, padding: 10, borderRadius: 20}}>
+                        <Text style={{color: '#b62a2a', textAlign: 'center'}}>
+                            Sair
+                        </Text>
+                    </View>
                 </View>
-                <CardPessoa
-                    titulo="Fisioterapeuta Responsável"
-                    pessoa={fisio}
-                    pessoaEmail={fisioEmail}
-                />
-                <CardPessoa
-                    titulo="Coordenador Responsável"
-                    pessoa={coord}
-                    pessoaEmail={coordEmail}
-                />
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -67,7 +128,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         backgroundColor: '#111827',
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
     },
     titulo: {
         color: '#F9FAFB',
